a53e20826ea33bf49385b7fbd17ccd0fea302c4f
chinniacharis <schinniachari@outlook.com>
2021-11-13 17:25:38 +0530

    Screen cards are done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'market-analytics-dashboard';
+  data: [{ header: string; title: string; text: string }] = [
+    {
+      header: 'Invoices - This Month',
+      title: '$75.98K',
+      text: '% of difference From: 0.52%',
+    },
+  ];
+  ngOnInit() {
+    this.data.push({
+      header: 'Invoices - This Month',
+      title: '$75.98K',
+      text: '% of difference From: 0.52%',
+    });
+    this.data.push({
+      header: 'Invoices - This Month',
+      title: '$75.98K',
+      text: '% of difference From: 0.52%',
+    });
+    this.data.push({
+      header: 'Invoices - This Month',
+      title: '$75.98K',
+      text: '% of difference From: 0.52%',
+    });
+    this.data.push({
+      header: 'Invoices - This Month',
+      title: '$75.98K',
+      text: '% of difference From: 0.52%',
+    });
+  }
 }
